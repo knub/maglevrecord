01898a42b7f50e50ca81adb71282a0f6548964b8
Stefan Bunk <stefan.bunk@googlemail.com>
2013-01-08 14:56:25 +0100

    Second gem version with notes for not using it.

--- test/test_maglevrecord.rb
+++ test/test_maglevrecord.rb
@@ -1,8 +1,14 @@
 require "test/unit"
 require "maglev_record"
+require "example_model"
 
 class MagLevRecordTest < Test::Unit::TestCase
   include ActiveModel::Lint::Tests
+
+  def model
+    Book.dummy
+  end
+
   def test_the_truth
     assert true
   end
