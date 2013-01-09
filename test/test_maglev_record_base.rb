a598d6e4af48e26a35b0d28432402c5d8c072ccd
niccokunzmann <Nicco.Kunzmann@student.hpi.uni-potsdam.de>
2013-01-09 11:33:57 +0100

    weg

--- test/test_maglev_record_base.rb
+++ test/test_maglev_record_base.rb
@@ -1,15 +1,15 @@
 require 'bacon'
 require 'maglev_record'
 
-class ExampleStudent
-  include Maglev::Base
-
-  attr_accessible :name
-
-  def initialize(name)
-    name = name
-  end
-end
+#class ExampleStudent
+#  include Maglev::Base
+#
+#  attr_accessible :name
+#
+#  def initialize(name)
+#    name = name
+#  end
+#end
 
 #describe 'ExampleStudent' do
 #  let(:student) { ExampleStudent.new('Ferdinand') }
