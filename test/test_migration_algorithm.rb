8781393674e34965bfa2563fbcb53b7218f60bac
niccokunzmann <Nicco.Kunzmann@student.hpi.uni-potsdam.de>
2013-01-18 10:46:25 +0100

    wrote tests for loading migrations from source

--- test/test_migration_algorithm.rb
+++ test/test_migration_algorithm.rb
@@ -485,4 +485,59 @@ class TestMigrationGraph < Test::Unit::TestCase
   end
 end
 
+class TestMigrationList_load_migrations < TestMigrationListBase
 
+  def test_load_simple_migration
+    h = l.load_source "
+      l = []
+      m = migration(1).follows(first_migration)
+      m.up{ l << 1 } unless m.has_up?
+      m.down{ assert l.delete(1) == 1 } unless m.has_down?
+    "
+    list = h["l"]
+    m = h["m"]
+    assert m.is_migration?
+    assert ! list.is_migration?
+    assert_equal list, []
+    assert l.migration_set.include? m
+  end
+
+  def test_load_several_migrations
+    h1 = l.load_source "
+      # TODO: discuss this, could be unliked
+      migration(2).follows(first_migration).when_created{ |migration|
+        assert migration.timestamp == 2
+        l = []
+        migration.up{
+          l << 2
+        }
+        migration.down{
+          assert l.delete(2) != nil
+        }
+      }
+    "
+    s = "
+      l = []
+      m = migration(3).follows(migration(2))
+      m.up{ l << 3
+      } unless m.has_up?
+      m.down{l << 6} unless m.has_down?
+    "
+    h2 = l.load_source s
+    h3 = l.load_source s
+    l.up
+    assert_equal h1["l"], [2]
+    assert_equal h2["l"], [3]
+    assert_equal h3["l"], []
+    assert l.migration(2).done?
+    l2 = ML.new
+    l2.up
+    assert_equal h1["l"], []
+    assert_equal h2["l"], [3, 6]
+    assert_equal h3["l"], []
+    assert ! l.migration(2).done?
+  end
+
+
+  
+end
