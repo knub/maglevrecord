b3c6d859b24147a770bc3f6e9ff7e21dc8bc8337
Stefan Bunk <stefan.bunk@googlemail.com>
2013-02-12 15:34:07 +0100

    Now committing migrations after successful run.

--- lib/tasks/maglev_record.rb
+++ lib/tasks/maglev_record.rb
@@ -3,10 +3,17 @@ require "maglev_record"
 MIGRATION_FOLDER = "test/migration/dummy_migrations"
 namespace :db do
   task :migrate do
+    Maglev.abort_transaction
     Rake::Task['db:migrate'].clear
     loader = MaglevRecord::MigrationLoader.new
     loader.load_directory(MIGRATION_FOLDER)
     migrator = MaglevRecord::Migrator.new(loader.migration_list)
     migrator.up
+    begin
+      Maglev.commit_transaction
+    rescue TransactionError => e
+      puts Maglev::PERSISTENT_ROOT
+      puts e
+    end
   end
 end
