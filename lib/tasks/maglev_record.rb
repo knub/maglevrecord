445a0a35b5207976709a924d6e4435d09aaf02ea
Stefan Bunk <stefan.bunk@googlemail.com>
2013-02-12 15:18:18 +0100

    Clearing 'rake db:migrate' before defining it again, because standard behaviour is to add actions instead of replacing.

--- lib/tasks/maglev_record.rb
+++ lib/tasks/maglev_record.rb
@@ -3,6 +3,7 @@ require "maglev_record"
 MIGRATION_FOLDER = "test/migration/dummy_migrations"
 namespace :db do
   task :migrate do
+    Rake::Task['db:migrate'].clear
     loader = MaglevRecord::MigrationLoader.new
     loader.load_directory(MIGRATION_FOLDER)
     migrator = MaglevRecord::Migrator.new(loader.migration_list)
