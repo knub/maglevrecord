b8fb81bac59dcb4cbaf01b74f23445559905f2e8
Stefan Bunk <stefan.bunk@googlemail.com>
2013-02-19 11:51:29 +0100

    Implemented migration algorithm with new concept: Only apply migrations not already applied.

--- lib/maglev_record/migration/migrator.rb
+++ lib/maglev_record/migration/migrator.rb
@@ -1,3 +1,4 @@
+require "set"
 
 module MaglevRecord
   ##
@@ -6,24 +7,36 @@ module MaglevRecord
   # Therefore, it decides which migrations must be undone, and
   # which must be done and leaves the stone in the desired state.
   class Migrator
+
+    # Make these two classes persistable, as we need them for storing
+    # which migrations already ran.
+    SortedSet.maglev_persistable
+    Set.maglev_persistable
+
     def initialize(migration_list)
       @migration_list = migration_list
     end
 
     ##
-    # Applies the desired state of migrations.
-    def up
-      to_do = @migration_list
-      to_do.sort.each do |mig|
-        Logger.info("Doing '" + mig.name + "' from " + mig.timestamp.to_s)
-        mig.do
-      end
+    # Returns all migrations currently in the stone in the correct order.
+    def migration_store
+      Maglev::PERSISTENT_ROOT[:__migrations__] ||= SortedSet.new
     end
 
     ##
-    # Returns all migrations currently in the stone in the correct order.
-    def stone_migrations
-      Migration.all.sort
+    # Applies the desired state of migrations.
+    def up
+      Maglev.abort_transaction
+      to_do = @migration_list.reject do |mig|
+       migration_store.include?(mig.id)
+      end
+      Logger.info("Already applied all migrations.") if to_do.empty?
+      to_do.sort.each do |mig|
+        Logger.info("Doing '" + mig.name + "' from " + mig.timestamp.to_s)
+        mig.do
+        migration_store.add(mig.id)
+      end
+      Maglev.commit_transaction
     end
   end
 end
