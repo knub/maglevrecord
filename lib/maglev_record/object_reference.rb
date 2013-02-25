f79b3ee83ddfbc7ddd84399db96460c5302d775f
niccokunzmann <Nicco.Kunzmann@student.hpi.uni-potsdam.de>
2013-02-25 17:52:01 +0100

    now we have eigenclasses support for string_reference_path_to

--- lib/maglev_record/object_reference.rb
+++ lib/maglev_record/object_reference.rb
@@ -28,14 +28,14 @@ class Object
     return nil
   end
   
-  def __hash_key_value
+  def __hash_key_value(anObject)
     begin
       assoc_value = instance_variable_defined?(:@_st_value)
       assoc_key = instance_variable_defined?(:@_st_key)
     rescue NameError
       return nil
     end
-    return nil unless assoc_key and assoc_value
+    return nil unless assoc_key.equal?(anObject) or assoc_value.equal?(anObject)
     return [assoc_key, assoc_value]
   end
   
@@ -48,15 +48,39 @@ class Object
   end
   
   def __eigenclass_equal_to(anObject)
+    # breaks maglev if this is used by 
+    # maglev-ruby -e "require 'object_reference'; o = Object.new; start = Time.now; f = o.string_reference_path_to(true, 20); p start - Time.now; puts f"
+    return false if self.class.equal?(Array)
     eigenclass = class << self
       self
     end
+    puts "got it!"
     eigenclass.equal? anObject
   end
 
+  def __incuded_module_equal_to(anObject)
+    included_modules.any?{ |m| m.equal?(anObject)} if respond_to? :included_modules
+  end
+
   # format the reference path
-  def string_reference_path_to(to_object, length = 10, width_of_line = 80, trace = true)
-    reference_path = reference_path_to(to_object, length, trace = trace)
+  #   returns a human readable string
+  #
+  #   length
+  #     the maximum length of the path
+  #
+  #   width_of_line
+  #     the maximum size of a line
+  #
+  #   trace
+  #     true if reference_path_length shall be printed to stdout to trace the progress
+  #     false otherwise
+  #
+  #   pretty
+  #     true if module includes shall be detected
+  #     false if maglev internals shall be revealed
+  #
+  def string_reference_path_to(to_object, length = 10, width_of_line = 80, trace = true, pretty = true)
+    reference_path = reference_path_to(to_object, length, trace)
     return "" if reference_path.nil?
     lines = []
     left_column_size = 0
@@ -66,41 +90,47 @@ class Object
       object = reference_path[referenced_index]
       referenced = reference_path[referenced_index + 1]
       referenced_index += 1
-      name_of_referenced = '??'
+      name_of_referenced = '?? of'
       inspect_string = nil
       # switch case on displaying references
       begin 
-        #puts "#obj: #{object.inspect}  #{referenced.inspect} "
+        puts "#obj: #{object.inspect}  #{referenced.inspect} at: #{referenced_index}\n\n"
         # object --> class
         if object.class.equal?(referenced)
-          name_of_referenced = 'class'
+          name_of_referenced = 'class of'
         # class --> superclass
         elsif object.respond_to? :superclass and object.superclass.equal?(referenced)
-          name_of_referenced = 'superclass'
+          name_of_referenced = 'superclass of'
+        # included
+        elsif pretty and object.__incuded_module_equal_to(reference_path[referenced_index + 2])
+          name_of_referenced = 'included by'
+          referenced = reference_path[referenced_index + 2]
+          referenced_index += 2
         # OrderPreservingHashAssociation --> ...
-        elsif not object.__hash_key_value.nil?
-          assoc_key, assoc_value = object.__hash_key_value
+        elsif not object.__hash_key_value(referenced).nil?
+          assoc_key, assoc_value = object.__hash_key_value(referenced)
+          puts "lalalalala #{assoc_key} #{assoc_value}"
           if assoc_value.equal?(referenced)
-            name_of_referenced = 'value'
+            name_of_referenced = 'value of'
           elsif assoc_key.equal?(referenced)
-            name_of_referenced = 'key'
+            name_of_referenced = 'key of'
           end
         # ... --> OrderPreservingHashAssociation
-        elsif not referenced.__hash_key_value.nil?
-          assoc_key, assoc_value = referenced.__hash_key_value
+        elsif not referenced.__hash_key_value(referenced).nil?
+          assoc_key, assoc_value = referenced.__hash_key_value(referenced)
           inspect_string = "#{assoc_key.inspect} => #{assoc_value.inspect}"
-          name_of_referenced = 'assoc'
+          name_of_referenced = 'association in'
         # ... --> attribute
         elsif not (variable = object.__instance_variable_equal_to(referenced)).nil?
-          name_of_referenced = variable
+          name_of_referenced = variable.to_s + ' in'
         # [...] -> object at index
         elsif object.respond_to?(:find_index) and (index = object.find_index{ |element| element.equal?(referenced)})
-          name_of_referenced = "at #{index.inspect}"
+          name_of_referenced = "at #{index.inspect} of"
         # eigenclass
         elsif object.__eigenclass_equal_to(referenced)
-          name_of_referenced = "eigenclass"
+          name_of_referenced = "eigenclass of"
         elsif referenced.__eigenclass_equal_to(object)
-          name_of_referenced = "eigenbase"
+          name_of_referenced = "eigenbase of"
         end
       rescue Exception => e
         name_of_referenced = 'error'
@@ -119,7 +149,7 @@ class Object
     right_column_size = 10 if right_column_size < 0
     s = ""
     lines.reverse.each{ |line|
-      s += "#{line[1][0..right_column_size]} is \n#{line[0].rjust(left_column_size)} of "
+      s += "#{line[1][0..right_column_size]} is \n#{line[0].rjust(left_column_size)} "
     }
     return s + "#{reference_path[0].inspect}"
   end
