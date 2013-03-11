4fbaa25bb25a9801555943a77fd566fc01f48b51
Matthias Springer <me@matthiasspringer.de>
2013-03-11 16:55:44 +0100

    Refactored startup process.

--- lib/maglev_record/persistence.rb
+++ lib/maglev_record/persistence.rb
@@ -17,5 +17,19 @@ module MaglevRecord
   def self.reset
     Maglev.abort_transaction
   end
+
+  def self.make_modules_persistent
+    modules = [MaglevRecord]
+
+    while (!modules.empty?)
+      mod = modules.pop
+      
+      if !mod.maglev_persistable?
+        mod.maglev_persistable
+        modules += mod.constants.collect {|c| mod.const_get(c)}.find_all {|c| c.class == Module}
+      end
+    end
+  end
+
 end
 
