859454e6366bf7a5d7f2d56308e7649463286bfc
Stefan Bunk <stefan.bunk@googlemail.com>
2013-03-13 14:35:36 +0100

    Added MaglevRecord::Migrations again.

---
+++ lib/maglev_record/migration/migrator.rb
@@ -0,0 +1,44 @@
+require "set"
+require "logger"
+
+module MaglevRecord
+  ##
+  # Given a migration list (the desired applied migrations)
+  # this class migrates to these state.
+  # Therefore, it decides which migrations must be undone, and
+  # which must be done and leaves the stone in the desired state.
+  class Migrator
+
+    # Make these two classes persistable, as we need them for storing
+    # which migrations already ran.
+    SortedSet.maglev_persistable
+    Set.maglev_persistable
+
+    def initialize(migration_list)
+      @migration_list = migration_list
+    end
+
+    ##
+    # Returns all migrations currently in the stone in the correct order.
+    def migration_store
+      Maglev::PERSISTENT_ROOT[:__migrations__] ||= SortedSet.new
+    end
+
+    ##
+    # Applies the desired state of migrations.
+    def up
+      Maglev.abort_transaction
+      to_do = @migration_list.reject do |mig|
+       migration_store.include?(mig.id)
+      end
+      logger = Logger.new(STDOUT)
+      logger.info("Already applied all migrations.") if to_do.empty?
+      to_do.sort.each do |mig|
+        logger.info("Doing '" + mig.name + "' from " + mig.timestamp.to_s)
+        mig.do
+        migration_store.add(mig.id)
+      end
+      Maglev.commit_transaction
+    end
+  end
+end
