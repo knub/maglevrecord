363db17be6601fffc7ff0a3c01db2ded2ada9d80
Dimitri Korsch <korschdima@yahoo.de>
2013-03-19 18:22:21 +0100

    rails running well

--- lib/maglev_record/enumerable.rb
+++ lib/maglev_record/enumerable.rb
@@ -13,6 +13,11 @@ module MaglevRecord
         object_pool.size
       end
       def find_by_objectid(id)
+        if id.respond_to? :to_i
+          id = id.to_i 
+        else
+          raise "#{id} do not respond to :to_i!"
+        end
         object_pool[id]
       end
     end
