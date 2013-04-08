fef2d4c548c4df20e2a6f77946f40fff3d3c7595
niccokunzmann <Nicco.Kunzmann@student.hpi.uni-potsdam.de>
2013-04-08 10:24:24 +0200

    removed example rails project

--- test/migration/testproject/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Testproject::Application.config.secret_token = 'b5dcf75af96e2cf828ee4fc52f2b9fe5e378dbe2d9403f10eb4926acf24390d2225178367a19edb89e5c655ab3e9dfc875362d1e0e8ed011e9a63b5e8668c7e4'
