57434f1339d6d2cc55d2f2eeca7be7e905bc07a8
niccokunzmann <Nicco.Kunzmann@student.hpi.uni-potsdam.de>
2013-04-12 15:56:21 +0200

    added tests for 3 times rename that were successful

--- test/migration/test_operations_fit_together.rb
+++ test/migration/test_operations_fit_together.rb
@@ -5,19 +5,69 @@ require 'time'
 
 ################## make sure renamed classes can be migrated even if they are not yet existent
 #
-# TODO
-#
-# imagine a system with out a class A and the following migrations to be applied:
-#
-# rename A to B
-# rename B to C
-#
-# this must work!
+#   TODO
+#   ====
 #
 # 2) von rooted base nach base
 #
+#   DONE
+#   ====
+#
+# 1) imagine a system with out a class A and the following migrations to be applied:
+#
+#    rename A to B
+#    rename B to C
+#
+#
 # 3) fail silently vs. do not execute
+#    chose 3.2
 #    3.1) Migration touches :ModelClass => not executed if not present
 #    3.2) ModelClass contant returns Null Object that can be migrated but does nothing
+
+class TestMigrateWithRenamingWorks < Test::Unit::TestCase
+
+  def setup
+    Object.module_eval "
+    class A 
+      include MaglevRecord::RootedBase
+    end
+    "
+  end
+  
+  def migrations
+    [
+      MaglevRecord::Migration.new(Time.now, "rename A to B") do
+        def up
+          rename_class A, :B
+        end
+      end,
+      MaglevRecord::Migration.new(Time.now + 1, "rename B to C") do
+        def up
+          rename_class B, :C
+        end
+      end,
+      MaglevRecord::Migration.new(Time.now + 2, "rename C to D") do
+        def up
+          rename_class C, :D
+        end
+      end
+    ].sort
+  end
+  
+  def do_migrations
+    migrations.each { |migration|
+      migration.do
+    }
+  end
+
+  def test_can_rename_all_migrations
+    class_A = A
+    do_migrations
+    assert_equal class_A, D
+  end
+
+end
+
+
 
 
