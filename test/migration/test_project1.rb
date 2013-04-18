1cb8873c318c3f542c458eaff07265dbd71e5d2f
niccokunzmann <Nicco.Kunzmann@student.hpi.uni-potsdam.de>
2013-04-18 11:20:41 +0200

    now test for project 1 tests also existence of rakefile and working directory

--- test/migration/test_project1.rb
+++ test/migration/test_project1.rb
@@ -18,7 +18,7 @@ class TempdirTest < Test::Unit::TestCase
 
     def shutdown
       # remove the directory.
-      FileUtils.remove_entry tempdir
+      FileUtils.remove_dir tempdir
     end
 
     def tempdir
@@ -26,12 +26,45 @@ class TempdirTest < Test::Unit::TestCase
     end
   end
 
+  def setup
+    @project_name = 'project1' if @project_name.nil?
+    # copy the project over to the tempdir
+    FileUtils.cp_r(project_source_directory, tempdir)
+    # change to the project directory to easyly run rake
+    @original_cwd = FileUtils.getwd
+    FileUtils.chdir(project_directory)
+  end
+
+  def teardown
+    FileUtils.chdir(@original_cwd)
+    FileUtils.remove_dir(project_directory)
+  end
+
+  def project_directory
+    File.join(tempdir, @project_name)
+  end
+
+  def project_source_directory
+    File.join(File.dirname(__FILE__), 'projects', @project_name)
+  end
+
   def tempdir
     self.class.tempdir
   end
 
+  # test the project configuration
+
   def test_tempdir_exists
-    File.directory? tempdir
+    assert File.directory?(tempdir)
+  end
+
+  def test_there_is_a_rakefile_in_the_project_directory
+    assert File.file?(File.join(project_directory, 'Rakefile'))
+    assert File.file?(File.join(project_source_directory, 'Rakefile'))
+  end
+
+  def test_cwd_is_project_directory
+    assert_equal FileUtils.getwd, project_directory
   end
 
 end
