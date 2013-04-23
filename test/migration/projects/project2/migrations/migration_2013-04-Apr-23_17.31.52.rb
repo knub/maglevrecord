e4bd2228dac572556403e40eb202aad253449129
niccokunzmann <Nicco.Kunzmann@student.hpi.uni-potsdam.de>
2013-04-23 17:32:48 +0200

    added three new migrations that shall be executed

---
+++ test/migration/projects/project2/migrations/migration_2013-04-Apr-23_17.31.52.rb
@@ -0,0 +1,16 @@
+require "maglev_record/migration"
+require "time"
+
+MaglevRecord::Migration.new(Time.parse('Tue Apr 23 17:31:52 +0000 2013'), 'fill in description here') do
+
+  def up
+    # put your transformation code here
+  end
+
+  def down
+    # put the code that reverses the code in up here 
+    # remove the next line that throws he error 
+    raise IrreversibleMigration, 'The migration has no downcode'
+  end
+
+end
