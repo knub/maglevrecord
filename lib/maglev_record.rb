0fb4b2084e4052abae3fd4241e16ccbcfc159d39
Stefan Bunk <stefan.bunk@googlemail.com>
2013-04-24 11:27:57 +0200

    Removed some useless requires from maglev_record.

--- lib/maglev_record.rb
+++ lib/maglev_record.rb
@@ -1,25 +1,23 @@
 
-require "active_model/naming"
 require "active_model"
+require "active_support"
 
 require "active_support/core_ext/class/attribute"
-require "maglev_record/maglev_support/active_support_patch"
-require "active_support/dependencies"
+require "maglev_record/maglev_support/maglev_support"
 require "bundler/setup"
-require "tsort"
 
 require "maglev_record/maglev_support/concern"
 require "maglev_record/tools"
 require "maglev_record/maglev_record"
 
 require "maglev_record/enumerable"
-require "maglev_record/errors"
+# require "maglev_record/errors"
 require "maglev_record/integration"
 require "maglev_record/persistence"
 require "maglev_record/read_write"
 require "maglev_record/rooted_persistence"
 
-require "maglev_record/migration"
+# require "maglev_record/migration"
 
 require "maglev_record/base"
 require "maglev_record/rooted_base"
