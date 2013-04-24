8d92909fadedd5cc736a5699bac1265807a5eba5
Stefan Bunk <stefan.bunk@googlemail.com>
2013-04-24 16:43:05 +0200

    Recursively copying the reinclude_store into new modules.

--- lib/maglev_record/maglev_support/concern.rb
+++ lib/maglev_record/maglev_support/concern.rb
@@ -4,15 +4,11 @@ module MaglevSupport
       self.included_modules.reverse.each do |mod|
         base.extend(mod::ClassMethods) if mod.constants.include? 'ClassMethods'
       end
+      self.reinclude_store.each do |mod|
+        base.__save_for_reinclude(mod)
+      end unless self.reinclude_store.nil?
+      # puts "self: #{self}, reinclude_store: #{self.reinclude_store}"
       base.extend(self::ClassMethods) if self.constants.include? 'ClassMethods'
-      (base.ancestors + [base]).each do |klass|
-        begin
-          # klass.maglev_persistable
-        rescue Exception => e
-          puts "====>Failed on #{klass}"
-          raise e
-        end
-      end
       if base.is_a? Class #&& (self == MaglevRecord::RootedBase || self == MaglevRecord::Base)
         # base.send :include, ActiveModel::Validations
         base.send :extend, Enumerable
