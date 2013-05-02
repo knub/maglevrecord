fe38a1c021f6a4baf3bb4c9aee3e04e1da4567f8
Stefan Bunk <stefan.bunk@googlemail.com>
2013-05-02 14:26:56 +0200

    Removed unnecessary outcommented requires.

--- lib/maglev_record.rb
+++ lib/maglev_record.rb
@@ -5,40 +5,8 @@ require "active_support/core_ext/class/attribute"
 
 require "maglev_record/maglev_support/maglev_support"
 
-# require "active_support/dependencies"
-# require "maglev_record/maglev_support/active_support_patch"
-
-# require "bundler/setup"
-# require "tsort"
-
-# require "singleton"
-# require "rake"
-# require "psych"
-# require "i18n"
-
-# require "maglev_record/maglev_support/concern"
-# require "maglev_record/maglev_support/secure_password"
-# require "maglev_record/tools"
-# require "maglev_record/maglev_record"
-
-# require "maglev_record/enumerable"
-# require "maglev_record/errors"
-# require "maglev_record/integration"
-# require "maglev_record/persistence"
-# require "maglev_record/read_write"
-# require "maglev_record/rooted_enumerable"
-# require "maglev_record/rooted_persistence"
-# require "maglev_record/sensible"
-
-# require "maglev_record/migration"
-
-# require "maglev_record/base"
-# require "maglev_record/rooted_base"
-
 require "maglev_record/tools"
-
 require "bundler/setup"
-
 require "maglev_record/maglev_support/concern"
 require "set"
 require "logger"
@@ -59,6 +27,9 @@ else
   puts "IT IS NOT DEFINED"
   # require "maglev_record/tools"
   require "maglev_record/maglev_record"
+  require "maglev_record/maglev_support/secure_password"
+  require "maglev_record/sensible"
+  require "maglev_record/rooted_enumerable"
   require "maglev_record/enumerable"
   require "maglev_record/errors"
   require "maglev_record/integration"
