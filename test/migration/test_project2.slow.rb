affacd0a4248b274ca2a2e3bcf7f5b47af8426af
niccokunzmann <Nicco.Kunzmann@student.hpi.uni-potsdam.de>
2013-05-02 20:27:00 +0200

    renamed the tested class to ProjectModel

--- test/migration/test_project2.slow.rb
+++ test/migration/test_project2.slow.rb
@@ -5,22 +5,22 @@ class MigrationProject2 < ProjectTest
   def setup
     @project_name = 'project2'
     super
-    Object.remove_const :TestModel if defined?(TestModel)
+    Object.remove_const :ProjectModel if defined?(ProjectModel)
     Maglev.commit_transaction
   end
 
   def test_no_models
     Maglev.abort_transaction
     assert_raise(NameError) {
-      TestModel
+      ProjectModel
     }
   end
 
   def test_models_appear
-    s = rails_c("puts TestModel\nMaglev.commit_transaction")
+    s = rails_c("puts ProjectModel\nMaglev.commit_transaction")
     p s
     Maglev.abort_transaction
-    assert_equal TestModel.all, []
+    assert_equal ProjectModel.all, []
   end
 
 end
