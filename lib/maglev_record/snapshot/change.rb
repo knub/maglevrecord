f93ee051aae0464533646cfc4bb510d4e3ba6599
niccokunzmann <Nicco.Kunzmann@student.hpi.uni-potsdam.de>
2013-05-23 12:30:40 +0200

    put SuperclassMismatchChange into own file

--- lib/maglev_record/snapshot/change.rb
+++ lib/maglev_record/snapshot/change.rb
@@ -85,49 +85,4 @@ module MaglevRecord
     end
 
   end
-
-  class SuperclassMismatchChange
-    "new_class_names new_classes 
-    removed_class_names removed_classes changed_classes changed_class_names
-    ".split.each do |name|
-      define_method(name){ [] }
-    end
-
-    def initialize(cls, file_path)
-      @cls = cls
-      @file_path = file_path
-    end
-
-    def nothing_changed?
-      false
-    end
-
-    def migration_string(identation = 0)
-      " " * identation + ["# TypeError: superclass mismatch for #{class_name}",
-       "# in #{@file_path}",
-       "#{class_name}.remove_superclass"].join("\n" + " " * identation)
-    end
-
-    def superclass_mismatch_classes
-      [self]
-    end
-
-    def superclass_mismatch_class_names
-      superclass_mismatch_classes.map(&:class_name)
-    end
-
-    # methods for the single change
-
-    def class_name
-      mismatching_class.name
-    end
-
-    def mismatching_class
-      @cls
-    end
-
-    def changes_since(snapshot)
-      self
-    end
-  end
 end
--- lib/maglev_record/snapshot/snapshot.rb
+++ lib/maglev_record/snapshot/snapshot.rb
@@ -1,6 +1,7 @@
 require "maglev_record/snapshot/snapshotable"
 require "maglev_record/snapshot/class_snapshot"
 require "maglev_record/snapshot/change"
+require "maglev_record/snapshot/superclass_mismatch_change"
 
 module MaglevRecord
 
